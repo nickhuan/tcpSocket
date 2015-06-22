f7df9de792c2850146bf9b23e3d4c0cb51401b47
nickhuan <daizhonghuan@gmail.com>
2015-06-22 15:27:24 +0200

    added receiveKeypoints function to return a vector<DavisKeypoint> and cleaned code

--- client.cpp
+++ client.cpp
@@ -3,18 +3,10 @@
 #include <opencv2/core.hpp>
 #include "tcpClient.hpp"
 
-
 using namespace std;
 using namespace cv;
 
 /* Struct and Class */
-struct test
-{
-  int x = 0;
-  int y = 0;
-  int idx = 0;
-  bool flag = 0;
-};
 struct DavisKeypoint
 {
   Point2f coordinate = Point2f(0,0);
@@ -29,71 +21,31 @@ struct DavisKeypointPacketHeader
   FrameType flag = EVENT;
   size_t number_of_keypoints = 0;
 };
-class DavisKeypointPacket
+
+vector<DavisKeypoint> receiveKeypoints(tcpClient client)
 {
-public:
-
   DavisKeypointPacketHeader header;
-  vector<DavisKeypoint> keypoints_davis;
+  client.receivePacket(&header, sizeof(header));
+  cout << "header flag: " << header.flag << endl;
+  cout << "number of keypoints: " << header.number_of_keypoints << endl;
 
-  DavisKeypointPacket(int n = 0)
-  : keypoints_davis(n)
-  { header.number_of_keypoints = n; }
+  vector<DavisKeypoint> keypoints_vector(header.number_of_keypoints);
+    for (auto &&kp: keypoints_vector)
+  {
+    client.receivePacket(&kp, sizeof(DavisKeypoint));
+  }
+  return keypoints_vector;
 };
 
 int main( int argc, char** argv )
 {
   /* TCP Client Setup */
   tcpClient client("127.0.0.1", 9999);
-
-  // int BUFLEN = 512;
-  // char buffer[BUFLEN];
-  // client.receiveMsg(buffer);
-  // cout << "message received: " << buffer << endl;
-
-  // /* send packet to server */
-  // test testPacket;
-  // testPacket.x = 52;
-  // testPacket.y = 78;
-  // testPacket.flag = 1;
-  // cout << "size of packet:" << sizeof(testPacket) << endl;
-  // client.sendPacket(&testPacket, sizeof(testPacket));
-
-  /* receive packet from server */
-  // void* dataBuffer = malloc(sizeof(DavisKeypointPacketHeader));
-  // client.receivePacket(dataBuffer, sizeof(struct DavisKeypointPacketHeader)); //receive packet header
-  // DavisKeypointPacketHeader* headerBuffer = (DavisKeypointPacketHeader*) dataBuffer;
-  // cout << "DavisKeypointPacket.flag: " << headerBuffer->flag << endl;
-  // cout << "DavisKeypointPacket.flag: " << headerBuffer->number_of_keypoints << endl;
   
-  DavisKeypointPacketHeader header;
-  client.receivePacket(&header, sizeof(header));
-  cout << "header flag: " << header.flag << endl;
-  cout << "number of keypoints: " << header.number_of_keypoints << endl;
+  vector<DavisKeypoint> keypoints_vector = receiveKeypoints(client);
 
-  // int n = headerBuffer->number_of_keypoints;
-  // vector<DavisKeypoint> keypoints(n);
-  // client.receivePacket( &keypoints[0], n * sizeof(DavisKeypoint) );
-
-  // client.receivePacket(headerBuffer + sizeof(struct DavisKeypointPacketHeader),
-  //                 headerBuffer->number_of_keypoints * sizeof(DavisKeypoint));
-  // DavisKeypoint* keypoint0 = (DavisKeypoint*) (headerBuffer + sizeof(struct DavisKeypointPacketHeader));
-  // cout << "keypoint0: " << keypoint0->coordinate << endl;
-  // DavisKeypoint* keypoint1 = (DavisKeypoint*) (headerBuffer + sizeof(struct DavisKeypointPacketHeader) + 1 * sizeof(DavisKeypoint));
-  // cout << "keypoint1: " << keypoint1->coordinate << endl;
-  // DavisKeypoint* keypoint2 = (DavisKeypoint*) (headerBuffer + sizeof(struct DavisKeypointPacketHeader) + 2 * sizeof(DavisKeypoint));
-  // cout << "keypoint2: " << keypoint2->coordinate << endl;
-  // DavisKeypoint* keypoint3 = (DavisKeypoint*) (headerBuffer + sizeof(struct DavisKeypointPacketHeader) + 3 * sizeof(DavisKeypoint));
-  // cout << "keypoint3: " << keypoint3->coordinate << endl;
-  // DavisKeypoint* keypoint4 = (DavisKeypoint*) (headerBuffer + sizeof(struct DavisKeypointPacketHeader) + 4 * sizeof(DavisKeypoint));
-  // cout << "keypoint4: " << keypoint4->coordinate << endl;
-  // DavisKeypointPacket* packet = (DavisKeypointPacket*) dataBuffer;
-  // cout << "in the packet: " << packet->keypoints_davis.at(0).coordinate << endl;
-  // cout << "address of packet: " << packet << endl;
-  // cout << "address of first vector" << &(packet->keypoints_davis) << endl;
-
-  // cout << "DavisKeypointPacket.at1.idx: " << headerBuffer->keypoints_davis.at(0).idx << endl;
-  // cout << "DavisKeypointPacket.at1.x: " << headerBuffer->keypoints_davis.at(0).coordinate.x << endl;
+  cout << "in keypoints_vector at 3: " << keypoints_vector.at(3).coordinate << endl;
+  cout << "in keypoints_vector at 4: " << keypoints_vector.at(4).coordinate << endl;
 
   client.closeConnection();
 
--- server.cpp
+++ server.cpp
@@ -3,18 +3,10 @@
 #include <opencv2/core.hpp>
 #include "tcpServer.hpp"
 
-
 using namespace std;
 using namespace cv;
 
 /* Struct and Class */
-struct test
-{
-  int x = 0;
-  int y = 0;
-  int idx = 0;
-  bool flag = 0;
-};
 struct DavisKeypoint
 {
   Point2f coordinate = Point2f(0,0);
@@ -31,61 +23,30 @@ struct DavisKeypointPacketHeader
 };
 
 void sendKeypoints(tcpServer server, FrameType frame_type,
-  const vector<DavisKeypoint> &keypoints_vector) {
+                  const vector<DavisKeypoint> &keypoints_vector)
+{
   DavisKeypointPacketHeader bufferHeader;
   bufferHeader.number_of_keypoints = keypoints_vector.size();
   bufferHeader.flag = frame_type;
   server.sendPacket(&bufferHeader, sizeof(bufferHeader));
 
-  for (DavisKeypoint kp : keypoints_vector) {
+  // for each kp in keypoints_vector
+  for (auto &&kp: keypoints_vector)
+  {
+    server.sendPacket(&kp, sizeof(DavisKeypoint));
   }
 };
 
 int main( int argc, char** argv )
 {
 	tcpServer server(9999);
-	// server.sendMsg("hello client, I'm server.");
-
- //  // test testPacket;
- //  // struct test structTest;
- //  // cout << "size of packet:" << sizeof(testPacket) << endl;
- //  // test *buffer;
- //  size_t bufferSize = 16;
- //  test *buffer = (test*) malloc(bufferSize);
- //  // test receivedPacket;
- //  // cout << "1";
- //  server.receivePacket(buffer, bufferSize);
- //  cout << "received" << endl;
- //  cout << "received packet.x: " << buffer->x << endl;
- //  cout << "received packet.y: " << buffer->y << endl;
- //  cout << "received packet.idx: " << buffer->idx << endl;
- //  cout << "received packet.flag: " << buffer->flag << endl;
 
   vector<DavisKeypoint> keypoints_vector(5);
+  keypoints_vector.at(3).coordinate = Point2f (33,56);
   cout << "size of keypoints_vector: " << sizeof(keypoints_vector) << endl;
   cout << "keypoints_vector.size(): " << keypoints_vector.size() << endl;
+
   sendKeypoints(server, FRAME, keypoints_vector);
-
-  // DavisKeypointPacket classPacket1;
-  // cout << "size of class DavisKeypointPacket: " << sizeof(classPacket1) << endl; 
-
-  // DavisKeypointPacket classPacket2(2);
-  // cout << "size of DavisKeypointPacketHeader: " << sizeof(DavisKeypointPacketHeader) << endl; 
-  // cout << "size of DavisKeypoint object: " << sizeof(DavisKeypoint) << endl; 
-  // cout << "size of class DavisKeypointPacket with vector size of 2: " << sizeof(classPacket2) << endl; 
-  // DavisKeypointPacket classPacket0(0);
-  // cout << "size of class DavisKeypointPacket with vector size of 0: " << sizeof(classPacket0) << endl; 
-  // DavisKeypointPacket classPacket5(5);
-  // classPacket5.keypoints_davis.at(3).coordinate = Point2f (23,56);
-  // cout << "size of class DavisKeypointPacket with vector size of 5: " << sizeof(classPacket5) << endl; 
-  // cout << "size of vector of 5: " << sizeof(classPacket5.keypoints_davis) << endl; 
-  // // std::vector<int> v;
-  // // cout << "size of vector int: " << sizeof(v) << endl;
-  // cout << "header flag: " << classPacket5.header.flag << endl;
-  // cout << "number keypoints: " << classPacket5.header.number_of_keypoints << endl;   
-  // cout << "vector size: " << classPacket5.keypoints_davis.size() << endl; 
-
-  // server.sendPacket(&classPacket5, sizeof(classPacket5));
 
   server.closeConnection(); 
 
